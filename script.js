21cdd77f293b3c04b42d6f70b8d77ac507d7013c
mahaltrupti <truptimahal2004@gmail.com>
2025-08-27 13:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+  <script>
+    // Simple interactive JS
+    document.querySelector("header h1").addEventListener("click", () => {
+      alert("Welcome to Accio Jobs 🚀");
+    });
+  </script>
